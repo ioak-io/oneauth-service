f1128177f1d2a117ca64940728f051b0afe99cc9
Arun Kumar Selvaraj <dev@ioak.org>
2020-12-30 17:43:51 +0530

    Automated startup script to initialize email templates

--- app/email_template/service.py
+++ app/email_template/service.py
@@ -13,3 +13,14 @@ def compose_message(template_name, arg):
         return message
     else:
         return ''
+
+def find_template(template_name):
+    results = db_utils.find(self_space, domain, {'name': template_name})
+    if len(results) > 0:
+        return results[0]
+    else:
+        return None
+
+def add_template(template_name, value):
+    outcome = db_utils.upsert(self_space, domain, {'name': template_name, 'value': value})
+    return outcome
--- oneauth/startup.py
+++ oneauth/startup.py
@@ -1,5 +1,20 @@
 import app.sequence.service as sequence_service
+from pathlib import Path
+import app.email_template.service as email_template_service
+
+static_dir = Path('static')
 
 def run():
     if sequence_service.is_present(100, 'spaceId', 'na') == False:
         sequence_service.create_sequence(100, 'spaceId', 'na', 210, 1)
+
+    confirm_email_link_existing_record = email_template_service.find_template('confirm_email_link')
+    reset_password_link_existing_record = email_template_service.find_template('reset_password_link')
+
+    if reset_password_link_existing_record == None:
+        reset_password_link_content = open(static_dir / 'email_templates/reset_password_link.html', 'r').read().replace('\n', ' ')
+        email_template_service.add_template('reset_password_link', reset_password_link_content)
+        
+    if confirm_email_link_existing_record == None:
+        confirm_email_link_content = open(static_dir / 'email_templates/confirm_email_link.html', 'r').read().replace('\n', ' ')
+        email_template_service.add_template('confirm_email_link', confirm_email_link_content)
