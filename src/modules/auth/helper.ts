18a4e5c15e9c98dab03705272e812f38a69d39e9
Arun Kumar Selvaraj <arun@ioak.io>
2021-10-11 23:28:25 +0530

    Fix to handle expired auth token

--- src/modules/auth/helper.ts
+++ src/modules/auth/helper.ts
@@ -177,10 +177,12 @@ export const deleteSessionByRefreshToken = async (
 export const decodeToken = async (token: string) => {
   const appRoot = process.cwd();
   const publicKey = fs.readFileSync(appRoot + "/public.pem");
+  console.log(publicKey);
   try {
     const res = await jwt.verify(token, publicKey);
     return { outcome: true, token, claims: res };
   } catch (err) {
+    console.log(err);
     return { outcome: false, err };
   }
 };
